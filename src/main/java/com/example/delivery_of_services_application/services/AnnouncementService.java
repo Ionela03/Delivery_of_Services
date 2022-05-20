50bd4fc5ba102a3f0408466788ec3ebff056e432
Ivascu_Ionela03 <ivascuionela949@yahoo.com>
2022-05-20 12:23:03 +0300

    DSA-28-make-all-it-is-necesarry-to-add-an-announcement-in-bata-base

--- src/main/java/com/example/delivery_of_services_application/services/AnnouncementService.java
+++ src/main/java/com/example/delivery_of_services_application/services/AnnouncementService.java
@@ -1,9 +1,13 @@
 package com.example.delivery_of_services_application.services;
 
 import com.example.delivery_of_services_application.exceptions.AnnouncementAlreadyExistsException;
+import com.example.delivery_of_services_application.exceptions.UsernameAlreadyExistsException;
 import com.example.delivery_of_services_application.users.Announcement;
+import com.example.delivery_of_services_application.users.User;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
+
+import java.util.Objects;
 
 import static com.example.delivery_of_services_application.services.FileSystemService.getPathToFile;
 
@@ -21,15 +25,18 @@ public class AnnouncementService {
 
 
     public static void addAnnouncement(String noAnnouncement,String county, String domain, String deplHome, String price, String negociablePrice, String phone, String description) throws AnnouncementAlreadyExistsException{
+        checkAnnouncementDoesNotExist(noAnnouncement);
         announcementRepository.insert(new Announcement(noAnnouncement, county, domain,deplHome,price,negociablePrice,phone,description ));
     }
 
     private static void checkAnnouncementDoesNotExist(String noAnnouncement) throws AnnouncementAlreadyExistsException {
-        for (Announcement ticket : announcementRepository.find()) {
-            if (noAnnouncement.equals(ticket.getNoAnnouncement())) {}
+        for (Announcement announcement : announcementRepository.find()) {
+            if (noAnnouncement.equals(announcement.getNoAnnouncement()))
                 throw new AnnouncementAlreadyExistsException(noAnnouncement);
         }
     }
+
+
 
     public static Announcement returnAnnouncement(String noAnnouncement){
         for (Announcement announcement : announcementRepository.find()){
