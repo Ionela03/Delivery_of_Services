8149ea6aa465e09482e58d50a6e8b178946ce6ac
Ivascu_Ionela03 <ivascuionela949@yahoo.com>
2022-05-22 15:00:04 +0300

    new data base added

---
+++ src/main/java/com/example/delivery_of_services_application/services/ChatService.java
@@ -0,0 +1,27 @@
+package com.example.delivery_of_services_application.services;
+
+import com.example.delivery_of_services_application.exceptions.AnnouncementAlreadyExistsException;
+import com.example.delivery_of_services_application.users.Announcement;
+import com.example.delivery_of_services_application.users.Chat;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+
+import static com.example.delivery_of_services_application.services.FileSystemService.getPathToFile;
+
+public class ChatService {
+
+    public static ObjectRepository<Chat> chatRepository;
+
+    public static void initChatDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("chatDatabase.db").toFile())
+                .openOrCreate("test", "test");
+
+        chatRepository = database.getRepository(Chat.class);
+    }
+
+    public static void initiateChat(String expeditor, String destinatar, String message, String status)  {
+
+        chatRepository.insert(new Chat(expeditor, destinatar,message,status));
+    }
+}
---
+++ src/main/java/com/example/delivery_of_services_application/users/Chat.java
@@ -0,0 +1,20 @@
+package com.example.delivery_of_services_application.users;
+
+public class Chat {
+
+    public String expeditor;
+    public String destinatar;
+    public String message;
+    public String status;
+
+    public Chat (String expeditor, String destinatar, String message, String status) {
+        this.expeditor = expeditor;
+        this.destinatar = destinatar;
+        this.message = message;
+        this.status = status;
+    }
+    public Chat(){
+
+    }
+
+}
