3395e8befbd41acc99dcbdc54380174ae0dac243
Ivascu_Ionela03 <ivascuionela949@yahoo.com>
2022-05-23 13:38:54 +0300

    DSA-25 Created a select Button for Announcement Details

--- src/main/java/com/example/delivery_of_services_application/controllers/AdsListController.java
+++ src/main/java/com/example/delivery_of_services_application/controllers/AdsListController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -26,6 +28,20 @@ public class AdsListController {
     private ListView<String> listView;
     @FXML
     private TextField username;
+
+    @FXML
+    Label name;
+    @FXML
+    Label locationLabel;
+    @FXML
+    Label displacement;
+    @FXML
+    Label price;
+    @FXML
+    Label negociablePriceLabel;
+
+    @FXML
+    Label description;
 
     @FXML
     void seeAnnouncementsButtonOnAction(ActionEvent actionEvent){
@@ -62,4 +78,23 @@ public class AdsListController {
         stage.show();
     }
 
+    public void selectButtonOnAction(ActionEvent actionEvent) {
+
+            String s= listView.getSelectionModel().getSelectedItem();
+            System.out.println("-"+s+"-");
+            for(Announcement ad: announcementRepository.find() )
+                if(Objects.equals(ad.noAnnouncement,s)){
+                    System.out.println("-"+ad.provider+"-");
+                    name.setText(ad.noAnnouncement);
+                    locationLabel.setText(ad.county);
+                    displacement.setText(ad.deplHome);
+                    price.setText(ad.price);
+                    negociablePriceLabel.setText(ad.negociablePrice);
+                    description.setText(ad.description);
+                }
+
+        }
+
+    public void deleteButtonOnAction(ActionEvent actionEvent) {
+    }
 }
